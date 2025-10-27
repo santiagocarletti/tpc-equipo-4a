faf79a1569af1cb86025e1cb1660acad0ed911f0
Nicolas Fernandezn <fernandeznn408@gmail.com>
2025-10-27 20:33:30 -0300

    Agregado de clases ComandaItemModificador, ComboDetalle y ProductoModificador

---
+++ dominio/ComandaItemModificador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class ComandaItemModificador
+    {
+        public int Id { get; set; }
+        public int ComandaItemId { get; set; }
+        public ComandaItem ComandaItem { get; set; }
+        public int ProductoModificadorId { get; set; }
+        public ProductoModificador ProductoModificador { get; set; }
+        public string Descripcion { get; set; } 
+        public decimal PrecioExtra { get; set; } 
+    }
+}
---
+++ dominio/ProductoModificador.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class ProductoModificador
+    {
+        public int Id { get; set; }
+        public int ProductoId { get; set; }
+        public Producto Producto { get; set; }
+        public string Descripcion { get; set; }
+        public decimal PrecioExtra { get; set; }
+        public bool Activo { get; set; } = true;
+    }
+}
